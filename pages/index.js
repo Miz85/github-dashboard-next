64df5937d45456778192c0be998b6207f258445d
Nazim Saouli <nazim.saouli@gmail.com>
2019-01-06 16:35:12 +0100

    Add now

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
+// import Link from "next/link";
 export default () => (
   <>
     <h1>Next Sandbox</h1>
     <div>
-      Hello World.{" "}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <span>Getting depoyed...</span>
     </div>
   </>
 );
