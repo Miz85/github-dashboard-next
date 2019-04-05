8cb84f7970e68d2b47a4c2bf74197ed4d4df145d
Nazim Saouli <nazim.saouli@gmail.com>
2019-04-06 00:57:14 +0200

    Initial authent + emotion

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,66 @@
-import Link from "next/link";
-export default () => (
-  <>
-    <h1>Next Sandbox</h1>
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  </>
-);
+// import Link from "next/link";
+import * as React from 'react';
+import { GithubLoginButton } from 'components/GithubLoginButton/GithubLoginButton';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import withData from 'lib/apollo';
+
+import fetch from 'node-fetch';
+
+class UserInfo extends React.Component {
+  render() {
+    return typeof window !== 'undefined' ? (
+      <Query
+        query={gql`
+          query {
+            viewer {
+              login
+              name
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          return (
+            <>
+              {loading ? <p>Loading...</p> : null}
+              {error ? <p>Error: {JSON.stringify(error)}</p> : null}
+              {!loading && !error && data ? <p>{data.viewer.login}</p> : null}
+            </>
+          );
+        }}
+      </Query>
+    ) : null;
+  }
+}
+
+class App extends React.Component {
+  state = {
+    isAuthenticated: false,
+    currentUser: null
+  };
+  componentDidMount() {
+    fetch(`${window.location.origin}/is-authenticated`)
+      .then(res => res.json())
+      .then(response =>
+        this.setState({
+          isAuthenticated: response.isAuthenticated
+        })
+      )
+      .catch(error => console.error(error));
+  }
+
+  render() {
+    const { isAuthenticated } = this.state;
+    return (
+      <>
+        <h1>Github Dashboard</h1>
+        <div>{!isAuthenticated && <GithubLoginButton />}</div>
+
+        {isAuthenticated && <UserInfo />}
+      </>
+    );
+  }
+}
+
+export default withData(App);
